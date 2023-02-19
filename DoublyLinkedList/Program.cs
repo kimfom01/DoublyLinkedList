e4d93784e21d24cb14b1579f4445742f39998a87
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-19 10:09:13 +0300

    Add sample code to use the list

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using DoublyLinkedList;
+
+Lesson2A();
+
+void Lesson2A()
+{
+    Random rnd = new Random();
+
+    DoubleLinkedList<int> list = new DoubleLinkedList<int>();
+
+    Console.WriteLine("Adding to the list...");
+
+    for (int i = 0; i < 10; ++i)
+    {
+        int nextValue = rnd.Next(100);
+
+        Console.Write("{0} ", nextValue);
+
+        bool added = false;
+
+        for (DoubleLinkedListNode<int> curr = list.Head; curr != null; curr = curr.Next)
+        {
+            if (nextValue < curr.Data)
+            {
+                list.AddBefore(curr, nextValue);
+                added = true;
+                break;
+            }
+        }
+
+        if (!added)
+        {
+            list.AddToEnd(nextValue);
+        }
+    }
+
+    Console.WriteLine();
+
+    Console.WriteLine("The sorted list is");
+    Console.WriteLine(ArrayToString(list.ToArray()));
+}
+
+string ArrayToString(Array array)
+{
+    StringBuilder sb = new StringBuilder();
+
+    sb.Append("[");
+    if (array.Length > 0)
+    {
+        sb.Append(array.GetValue(0));
+    }
+    for (int i = 1; i < array.Length; ++i)
+    {
+        sb.AppendFormat(", {0}", array.GetValue(i));
+    }
+
+    sb.Append("]");
+
+    return sb.ToString();
+}
